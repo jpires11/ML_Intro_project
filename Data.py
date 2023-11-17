42b59644f2aeaca221e95d89bcf3fe4289630cc6
jpires11 <joana.pires@epfl.ch>
2023-11-17 17:02:25 +0100

    first plot

--- Data.py
+++ Data.py
@@ -1,7 +1,28 @@
 
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 
 data= pd.read_csv('train.csv')
-print (data.columns)
+#third_column = data.iloc[:, 2]  # Accessing the third column 
+#print(third_column)
+import seaborn as sns
+#sns.boxplot(x='Lab', y='RT', data=data)# histogram
+#plt.xticks(rotation=90) 
+#plt.show()
+
+# Assuming 'data' is your DataFrame
+first_20_drugs = data['Compound'].value_counts().index[:50]  # Get the names of the first 10 drugs
+
+# Filter the data for the first 20 drugs
+filtered_data = data[data['Compound'].isin(first_20_drugs)]
+
+# Create the scatter plot
+sns.scatterplot(x='Compound', y='RT', hue='Lab', data=filtered_data)
+plt.xlabel('Drug Names (SMILES)')
+plt.ylabel('Retention Time (RT)')
+plt.title('Retention Time vs Drug Names by Lab (First 20 Drugs)')
+plt.xticks(rotation=90)  # Rotate x-axis labels if needed
+plt.legend(title='Lab', bbox_to_anchor=(1.05, 1), loc='upper left')  # Adjust legend position
+plt.tight_layout()
+plt.show()
